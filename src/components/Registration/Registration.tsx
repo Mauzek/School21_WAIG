ff69aa0235f363cd0df231f6535d6d3ca7ac8572
IcQxp <batla3080@gmail.com>
2024-10-25 19:38:53 +0300

    update

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -7,62 +7,60 @@ import sberKusya from "../../assets/images/SberKusya1.svg";
 
 export default function Registration() {
     const [currentStep, setCurrentStep] = useState<number>(1);
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
     const handleNextStep = () => {
         setCurrentStep(currentStep + 1);
     };
     const handlePreviousStep = () => {
         setCurrentStep(currentStep - 1);
     };
-    
+
 
     return (
-        <div className={styles["auth__container"]}>
+        <div className={styles.auth__container}>
             <p>Welcome to School 21 Chat</p>
             <form>
-                <div className={styles["form__container"]} style={{ alignContent: "center" }}>
-                    <div className={`${styles["input"]} ${currentStep === 1 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                <div className={styles.form__container} style={{ alignContent: "center" }}>
+                    <div className={`${styles.input} ${currentStep === 1 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Фамилия
                         </label>
-                        <div className={styles["input__container"]} >
-                            <input className={`${styles["input__box"]}`}>
+                        <div className={styles.input__container} >
+                            <input className={`${styles.input__box}`}>
                             </input>
                         </div>
                     </div>
-                    <div className={`${styles["input"]} ${currentStep === 1 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div className={`${styles.input} ${currentStep === 1 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Имя
                         </label>
                         <div>
-                            <input className={`${styles["input__box"]}`}>
+                            <input className={`${styles.input__box}`}>
                             </input>
                         </div>
                     </div>
-                    <div className={`${styles["input"]} ${currentStep === 1 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div className={`${styles.input} ${currentStep === 1 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Отчество
                         </label>
                         <div>
-                            <input className={`${styles["input__box"]}`}>
+                            <input className={`${styles.input__box}`}>
                             </input>
                         </div>
                     </div>
-                    <div className={`${styles["input"]} ${currentStep === 2 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div className={`${styles.input} ${currentStep === 2 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Дата рождения
                         </label>
                         <div>
                             <DatePicker style={{ border: "0", width: "330px", height: "48px" }} />
                         </div>
                     </div>
-                    <div style={{ height: "165px", border: "0", background: "transparent" }} className={`${styles["input"]} ${currentStep === 2 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div style={{ height: "165px", border: "0", background: "transparent" }} className={`${styles.input} ${currentStep === 2 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Пол
                         </label>
                         <div style={{ width: "100%", display: "flex", flexDirection: "row", height: "100%" }}>
-                            <div style={{ borderBottomLeftRadius: "8px", borderTopLeftRadius: "8px" }} className={styles["Gender__img"]} >
+                            <div style={{ borderBottomLeftRadius: "8px", borderTopLeftRadius: "8px" }} className={styles.Gender__img} >
                                 <label htmlFor="male" id="male2" style={{ zIndex: "1000", position: "absolute", width: "100%", height: "100%", bottom: "0", right: "0" }} />
                                 <input type="radio" id="male" name="sex" />
                                 <label htmlFor="male" id="male" />
@@ -74,7 +72,7 @@ export default function Registration() {
                                 width: "1px",
                                 border: "0"
                             }}></hr>
-                            <div style={{ borderBottomRightRadius: "8px", borderTopRightRadius: "8px" }} className={styles["Gender__img"]} >
+                            <div style={{ borderBottomRightRadius: "8px", borderTopRightRadius: "8px" }} className={styles.Gender__img} >
                                 <label htmlFor="female" id="female2" style={{ zIndex: "1000", position: "absolute", width: "100%", height: "100%", bottom: "0", right: "0" }} />
                                 <input type="radio" id="female" name="sex" />
                                 <label htmlFor="female" id="female"></label>
@@ -87,53 +85,53 @@ export default function Registration() {
                             </div>
                         </div>
                     </div>
-                    <div className={`${styles["input"]} ${currentStep === 3 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div className={`${styles.input} ${currentStep === 3 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Ник на платформе
                         </label>
                         <div>
-                            <input type="password" className={`${styles["input__box"]}`}>
+                            <input type="password" className={`${styles.input__box}`}>
                             </input>
                         </div>
                     </div>
-                    <div className={`${styles["input"]} ${currentStep === 3 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div className={`${styles.input} ${currentStep === 3 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Ник telegram
                         </label>
                         <div>
-                            <input type="password" className={`${styles["input__box"]}`}>
+                            <input type="password" className={`${styles.input__box}`}>
                             </input>
                         </div>
                     </div>
-                    <div className={`${styles["input"]} ${currentStep === 3 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div className={`${styles.input} ${currentStep === 3 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Email
                         </label>
                         <div>
-                            <input type="password" className={`${styles["input__box"]}`}>
+                            <input type="password" className={`${styles.input__box}`}>
                             </input>
                         </div>
                     </div>
-                    <div className={`${styles["input"]} ${currentStep === 4 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div className={`${styles.input} ${currentStep === 4 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Пароль
                         </label>
                         <div>
-                            <input type="password" className={`${styles["input__box"]}`}>
+                            <input type="password" className={`${styles.input__box}`}>
                             </input>
                         </div>
                     </div>
-                    <div className={`${styles["input"]} ${currentStep === 4 ? styles["input__active"] : styles["input__inactive"]}`}>
-                        <label className={styles["input__label"]}>
+                    <div className={`${styles.input} ${currentStep === 4 ? styles.input__active : styles.input__inactive}`}>
+                        <label className={styles.input__label}>
                             Подтверждение пароля
                         </label>
                         <div>
-                            <input type="password" className={`${styles["input__box"]}`}>
+                            <input type="password" className={`${styles.input__box}`}>
                             </input>
                         </div>
                     </div>
                 </div>
-                <button className={`${styles["input"]} ${currentStep === 5 ? styles["input__active"] : styles["input__inactive"]}`} > ss</button>
+                <button className={`${styles.input} ${currentStep === 5 ? styles.input__active : styles.input__inactive}`} > ss</button>
             </form>
             <div style={{
                 display: "flex",
@@ -141,10 +139,10 @@ export default function Registration() {
                 flexDirection: "row",
                 justifyContent: "space-between"
             }}>
-                <button className={`${styles["step__button"]}  ${currentStep !== 1 ? styles["input__active"] : styles["input__inactive"]}`} style={{ transform: "scale(-1,1)" }} onClick={handlePreviousStep}>
+                <button className={`${styles.step__button}  ${currentStep !== 1 ? styles.input__active : styles.input__inactive}`} style={{ transform: "scale(-1,1)" }} onClick={handlePreviousStep}>
                     <img src={regArrow} />
                 </button>
-                <button className={styles["step__button"]} onClick={handleNextStep}>
+                <button className={styles.step__button} onClick={handleNextStep}>
                     <img src={regArrow} />
                 </button>
             </div>
--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -7,37 +7,37 @@ export default function AuthPage() {
   const [isAuther, setIsAuther] = useState<boolean>(true);
   return (
     <>
-      <div className={styles["auth"]}>
+      <div className={styles.auth}>
         {isAuther ?
-          <div className={styles["auth__container"]}>
+          <div className={styles.auth__container}>
             <h2 style={{ fontSize: "24px", marginBottom: "16px", textAlign: "start" }}>Welcome to School 21 Chat</h2>
             <p style={{ fontSize: "14px", fontWeight: "400", textAlign: "justify" }}> Пожалуйста, введите свой логин и пароль, которые вы получили ранее
               для входа в систему </p>
             <form style={{ marginTop: "40px" }}>
-              <div className={styles["input"]}>
-                <label className={styles["input__label"]}>
+              <div className={styles.input}>
+                <label className={styles.input__label}>
                   login
                 </label>
-                <div className={styles["input__container"]} >
-                  <input className={`${styles["input__box"]}`}>
+                <div className={styles.input__container} >
+                  <input className={`${styles.input__box}`}>
                   </input>
                 </div>
               </div>
-              <div className={styles["input"]}>
-                <label className={styles["input__label"]}>
+              <div className={styles.input}>
+                <label className={styles.input__label}>
                   password
                 </label>
                 <div>
-                  <input type="password" className={`${styles["input__box"]}`}>
+                  <input type="password" className={`${styles.input__box}`}>
                   </input>
                 </div>
               </div>
 
 
-              <div className={styles["login__container"]}>
-                <button className={styles["login__Button"]} > <div>Войти</div>
+              <div className={styles.login__container}>
+                <button className={styles.login__Button} > <div>Войти</div>
 
-                  <div className={styles["login__Button__arrow"]}>
+                  <div className={styles.login__Button__arrow}>
                     <img src={loginSvg}>
                     </img>
                   </div>
